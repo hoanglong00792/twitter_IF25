a127d39feb587fcfe5beb28df5bbbf0229bf0851
hoanglong00792 <hoanglong00792@gmail.com>
2015-03-17 16:12:14 +0100

    modified:   .classpath 	modified:   src/twitter_IF25/GetStatus.java 	modified:   src/twitter_IF25/PrintSampleStream.java 	insert.sql

--- src/twitter_IF25/GetStatus.java
+++ src/twitter_IF25/GetStatus.java
@@ -1,138 +1,111 @@
 package twitter_IF25;
 
-import java.sql.Connection;
-import java.sql.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.commons.lang.StringEscapeUtils;
+
 
 import twitter4j.*;
 
+
+
 public class GetStatus {
-	static int stop = 0;
-	static int connect = 0;
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/IF25";
+	public static void write(String text) throws IOException {
+		BufferedWriter out = null;
 
-	// Database credentials
-	static final String USER = "root";
-	static final String PASS = "";
+		FileWriter fstream = new FileWriter("insert.sql", true); // true tells
+																	// to append
+																	// data.
+		out = new BufferedWriter(fstream);
+		try {
+			out.write(text+"\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.close();
+	}
+
 	public static void main(String[] args) throws TwitterException {
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-        StatusListener listener = new StatusListener() {
-            @Override
-            public void onStatus(Status status) {
-            	Connection conn = null;
-        		Statement stmt = null;
+		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+		StatusListener listener = new StatusListener() {
 
-        		try {
-    				System.out.println("a..."+stop);
+			@Override
+			public void onStatus(Status status) {
+				String sql1 = "INSERT INTO Status(Id,PersonId2,Created_at, Text, Geo, Place)"
+						+ " VALUES ( "
+						+ status.getId()
+						+ ","
+						+ status.getUser().getId()
+						+ ","
+						+ "'"
+						+ status.getCreatedAt()
+						+ "',"
+						+"'"+StringEscapeUtils.escapeSql(status.getText()) + "',"
+						+ status.getGeoLocation()
+						+ ","
+						+ status.getPlace() + ");";
+				System.out.println(sql1);
+				try {
+					GetStatus.write(sql1);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (status.getRateLimitStatus() != null) {
+					System.out
+							.println("status.getRateLimitStatus().getLimit() = "
+									+ status.getRateLimitStatus().getLimit());
+					System.out
+							.println("status.getRateLimitStatus().getRemaining() = "
+									+ status.getRateLimitStatus()
+											.getRemaining());
+					System.out
+							.println("status.getRateLimitStatus().getResetTimeInSeconds() = "
+									+ status.getRateLimitStatus()
+											.getResetTimeInSeconds());
+					System.out
+							.println("status.getRateLimitStatus().getSecondsUntilReset() = "
+									+ status.getRateLimitStatus()
+											.getSecondsUntilReset());
+					// System.out.println("status.getRateLimitStatus().getRemainingHits() = "
+					// +
+					// status.getRateLimitStatus().getRemainingHits());
+				}
+			}
 
-        			if (stop != 1) {
-        				// STEP 2: Register JDBC driver
-        				if (connect != 1) {
-        					Class.forName("com.mysql.jdbc.Driver");
+			@Override
+			public void onDeletionNotice(
+					StatusDeletionNotice statusDeletionNotice) {
+				// System.out.println("Got a status deletion notice id:" +
+				// statusDeletionNotice.getStatusId());
+			}
 
-        					// STEP 3: Open a connection
-        					System.out.println("Connecting to    database...");
-        					conn = DriverManager.getConnection(DB_URL, USER, PASS);
-        					// no need to display the fields... directly insert
-        					// them and work on it
-        					// System.out.println(status);
-        					// EDIT THIS
-        					System.out.println("Inserting records into the table...");
-        					stmt = conn.createStatement();
-        					connect = 1;
-        				}
-        				
-        				String sql1 =
-                                "INSERT INTO Status(Id,PersonId2,Created_at, Text, Geo, Place)"
-                                + " VALUES ( " + status.getId() +","
-                                +status.getUser().getId() +  ","
-                                +"'"+status.getCreatedAt() + "'," 
-                                //+"'"+status.getText().replace("\\", "\\\\") + "',"
-                                +"'',"
-                                +status.getGeoLocation()+ ","
-                                +status.getPlace()+ ");";
-        				System.out.println(sql1);
-        				stmt.executeUpdate(sql1);
-        				//System.exit(0);
+			@Override
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+				// System.out.println("Got track limitation notice:" +
+				// numberOfLimitedStatuses);
+			}
 
-        				System.out.println("Inserted records into the table...");
-        				if (status.getRateLimitStatus() != null) {
-                            System.out
-                                    .println("status.getRateLimitStatus().getLimit() = "
-                                            + status.getRateLimitStatus()
-                                                    .getLimit());
-                            System.out
-                                    .println("status.getRateLimitStatus().getRemaining() = "
-                                            + status.getRateLimitStatus()
-                                                    .getRemaining());
-                            System.out
-                                    .println("status.getRateLimitStatus().getResetTimeInSeconds() = "
-                                            + status.getRateLimitStatus()
-                                                    .getResetTimeInSeconds());
-                            System.out
-                                    .println("status.getRateLimitStatus().getSecondsUntilReset() = "
-                                            + status.getRateLimitStatus()
-                                                    .getSecondsUntilReset());
-                            // System.out.println("status.getRateLimitStatus().getRemainingHits() = "
-                            // +
-                            // status.getRateLimitStatus().getRemainingHits());
-                        }
-        				stop = 1;
-        				//System.exit(0);
-        				stmt.close();
-        				conn.close();
-        				System.out.println("Inserted records into the tableafafa..."+stop);
-        			}
-        		} catch (SQLException se) {
-        			// Handle errors for JDBC
-        			se.printStackTrace();
-        		} catch (Exception e) {
-        			// Handle errors for Class.forName
-        			e.printStackTrace();
-        		} finally {
-        			// finally block used to close resources
-        			try {
-        				if (stmt != null)
-        					stmt.close();
-        			} catch (SQLException se2) {
-        			}// nothing we can do
-        			try {
-        				if (conn != null)
-        					conn.close();
-        			} catch (SQLException se) {
-        				se.printStackTrace();
-        			}// end finally try
-        		}// end try
+			@Override
+			public void onScrubGeo(long userId, long upToStatusId) {
+				// System.out.println("Got scrub_geo event userId:" + userId +
+				// " upToStatusId:" + upToStatusId);
+			}
 
-            }
+			@Override
+			public void onStallWarning(StallWarning warning) {
+				// System.out.println("Got stall warning:" + warning);
+			}
 
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-                //System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-            }
+			@Override
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+		};
 
-            @Override
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-                //System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-            }
-
-            @Override
-            public void onScrubGeo(long userId, long upToStatusId) {
-                //System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-            }
-
-            @Override
-            public void onStallWarning(StallWarning warning) {
-                //System.out.println("Got stall warning:" + warning);
-            }
-
-            @Override
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-        };
-        twitterStream.addListener(listener);
-        twitterStream.sample();
-    }
-
+		twitterStream.addListener(listener);
+		twitterStream.sample();
+	}
 }
