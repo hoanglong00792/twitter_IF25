8be8dec8d1a81bfa32be5e2567afeb35de3d2290
hoanglong00792 <hoanglong00792@gmail.com>
2015-04-15 01:25:25 +0200

    modified:   .classpath 	modified:   src/twitter_IF25/AnalyseData.java 	deleted:    src/twitter_IF25/GetStatus.java 	deleted:    src/twitter_IF25/PrintSampleStream.java 	modified:   src/twitter_IF25/SearchTweets.java 	src/twitter_IF25/Fenetre.java 	src/twitter_IF25/Statistique.java 	src/twitter_IF25/StreamTweets.java 	src/twitter_IF25/Test.java 	src/twitter_IF25/User.java 	src/twitter_IF25/Visualisation.java 	src/twitter_IF25/test_api.java

--- src/twitter_IF25/AnalyseData.java
+++ src/twitter_IF25/AnalyseData.java
@@ -1,32 +1,33 @@
 package twitter_IF25;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
-import com.mongodb.BulkWriteOperation;
-import com.mongodb.BulkWriteResult;
-import com.mongodb.Cursor;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.ParallelScanOptions;
-import com.mongodb.ServerAddress;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
-import java.util.Set;
+import java.util.Observable;
 
 import org.apache.commons.lang.StringUtils;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 
-public class AnalyseData {
-	public static void main(String[] args) {
-
+public class AnalyseData extends Observable implements Runnable  {
+	@Override
+	public void run() {
 		MongoClient mongoClient = new MongoClient("localhost");
 
 		DB db = mongoClient.getDB("tweetdata");
@@ -45,94 +46,186 @@ public class AnalyseData {
 
 				BasicDBObject user = (BasicDBObject) tweet.get("user");
 
-				// =============================================================================
-				// ==============================Agressiveness==================================
-				// =============================================================================
-
-				// String tweet_created_at = (String)
-				// tweet.get("created_at");
-				String user_created_at = (String) user.get("created_at");
-				double friends_count = (double) user.get("friends_count");
-				// Double followers_count = (Double)
-				// user.get("followers_count");
-				double tweets_count = (double) user.get("statuses_count");
-
-				DateFormat inputFormat = new SimpleDateFormat(
-						"EEE MMM dd HH:mm:ss ZZZZZ yyyy", Locale.ENGLISH);
-				inputFormat.setLenient(true);
-				double diff = 1;
+				User new_user = new User();
+				new_user = AnalyseData.calculAgressiveness(user, tweet,
+						new_user);
+				new_user = AnalyseData.calculVisibility(user, tweet,
+						coll_users, new_user);
 				try {
-					Date user_date = inputFormat.parse(user_created_at);
-					Date now_date = new Date();
-					// System.out.println(now_date + "-" + user_created_at +
-					// ":");
-					diff = (double) (now_date.getTime() - user_date.getTime());
-					diff = diff / 3600000;
-
-				} catch (ParseException e) {
+					new_user = AnalyseData.calculDanger(user, tweet, new_user);
+				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				Double agressiveness = ((tweets_count + friends_count) / diff) / 350;
-				// System.out.println(tweets_count);
-				// System.out.println(friends_count);
-				// System.out.println(diff);
-				// System.out.println(agressiveness);
-
-				// ============================================================================
-				// ==============================VISIBILITY====================================
-				// ============================================================================
-
-				String text = (String) tweet.get("text");
-				int count_hashtag = StringUtils.countMatches(text, "#");
-				int count_mention = StringUtils.countMatches(text, "@");
-				int count_tweet_analysed = 1;
-
-				String id_user = (String) user.get("id_str");
-				DBCursor cursor_user = coll_users.find(new BasicDBObject(
-						"id_str", id_user));
-				try {
-					while (cursor_user.hasNext()) {
-						BasicDBObject user_old = (BasicDBObject) cursor_user
-								.next();
-						count_tweet_analysed = count_tweet_analysed
-								+ (int) user_old.get("count_tweet_analysed");
-						count_mention = count_mention
-								+ (int) user_old.get("count_mention");
-						count_hashtag = count_hashtag
-								+ (int) user_old.get("count_hashtag");
-						System.out.println("+++++++++++++++++" + user_old);
-						coll_users.remove(user_old);
-					}
-				} finally {
-					cursor_user.close();
-				}
-				// String screen_name = (String) user.get("screen_name");
-
-				double visibility = (count_hashtag / count_tweet_analysed
-						* 11.6 + count_mention / count_tweet_analysed * 11.4) / 140;
-				System.out.println(visibility + "**" + count_hashtag + "-"
-						+ count_mention + ":	" + text);
-
-				double danger = 0;
-				BasicDBObject doc = user.append("agressiveness", agressiveness)
-						.append("visibility", visibility)
-						.append("danger", danger)
-						.append("agressiveness", agressiveness)
-						.append("count_hashtag", count_hashtag)
-						.append("count_mention", count_mention)
-						.append("count_tweet_analysed", count_tweet_analysed);
+				BasicDBObject doc = user
+						.append("agressiveness", new_user.getAgressiveness())
+						.append("visibility", new_user.getVisibility())
+						.append("danger", new_user.getDanger())
+						.append("count_hashtag", new_user.getCount_hashtag())
+						.append("count_mention", new_user.getCount_mention())
+						.append("count_malware_link", new_user.getCount_malware_link())
+						.append("count_tweet_analysed",
+								new_user.getCount_tweet_analysed());
 				coll_users.insert(doc);
-				//String id_tweet = (String) tweet.get("_id");
-//				BasicDBObject searchQuery = new BasicDBObject().append(
-//						"_id", id_tweet);
 				tweet.append("read", 1);
-				coll_tweets.save( tweet);
+				coll_tweets.save(tweet);
 
 			}
 		} finally {
 			cursor.close();
 		}
 
+	}
+
+	public static User calculVisibility(BasicDBObject user,
+			BasicDBObject tweet, DBCollection coll_users, User new_user) {
+		// ============================================================================
+		// ==============================VISIBILITY====================================
+		// ============================================================================
+
+		String text = (String) tweet.get("text");
+		int count_hashtag = StringUtils.countMatches(text, "#");
+		int count_mention = StringUtils.countMatches(text, "@");
+		int count_tweet_analysed = 1;
+		int count_malware_link=0;
+		
+		String id_user = (String) user.get("id_str");
+		DBCursor cursor_user = coll_users.find(new BasicDBObject("id_str",
+				id_user));
+		try {
+			while (cursor_user.hasNext()) {
+				BasicDBObject user_old = (BasicDBObject) cursor_user.next();
+				count_tweet_analysed = count_tweet_analysed
+						+ (int) user_old.get("count_tweet_analysed");
+				count_mention = count_mention
+						+ (int) user_old.get("count_mention");
+				count_hashtag = count_hashtag
+						+ (int) user_old.get("count_hashtag");
+				count_malware_link = (int) user_old.get("count_malware_link");
+				System.out.println("+++++++++++++++++" + user_old);
+				coll_users.remove(user_old);
+			}
+		} finally {
+			cursor_user.close();
+		}
+		// String screen_name = (String) user.get("screen_name");
+
+		double visibility = (count_hashtag / count_tweet_analysed * 11.6 + count_mention
+				/ count_tweet_analysed * 11.4) / 140;
+		System.out.println(visibility + "**" + count_hashtag + "-"
+				+ count_mention + ":	" + text);
+
+		new_user.setVisibility(visibility);
+		new_user.setCount_hashtag(count_hashtag);
+		new_user.setCount_mention(count_mention);
+		new_user.setCount_tweet_analysed(count_tweet_analysed);
+		new_user.setCount_malware_link(count_malware_link);
+		
+		return new_user;
+
+	}
+
+	public static User calculDanger(BasicDBObject user, BasicDBObject tweet,
+			User new_user) throws IOException {
+		double danger = 0;
+		boolean is_malware=false;
+		int count_malware_link=new_user.getCount_malware_link();
+		BasicDBObject entities = (BasicDBObject) tweet.get("entities");
+		BasicDBList list_urls = (BasicDBList) entities.get("urls");
+		for (Object object : list_urls) {
+			DBObject embedded = (DBObject) object;
+			System.out.println(embedded.get("expanded_url"));
+			String link =(String) embedded.get("expanded_url");
+			if (AnalyseData.check_link(link)){
+				is_malware=true;
+			};
+		}
+		if (is_malware){
+			count_malware_link++;
+		}
+		int count_tweet_analysed=new_user.getCount_tweet_analysed();
+		danger=count_malware_link/count_tweet_analysed;
+		new_user.setDanger(danger);
+		return new_user;
+	}
+
+	public static User calculAgressiveness(BasicDBObject user,
+			BasicDBObject tweet, User new_user) {
+		// =============================================================================
+		// ==============================Agressiveness==================================
+		// =============================================================================
+
+		String user_created_at = (String) user.get("created_at");
+		int friends_count = (int) user.get("friends_count");
+		int tweets_count = (int) user.get("statuses_count");
+
+		DateFormat inputFormat = new SimpleDateFormat(
+				"EEE MMM dd HH:mm:ss ZZZZZ yyyy", Locale.ENGLISH);
+		inputFormat.setLenient(true);
+		double diff = 1;
+		try {
+			Date user_date = inputFormat.parse(user_created_at);
+			Date now_date = new Date();
+			diff = (double) (now_date.getTime() - user_date.getTime());
+			diff = diff / 3600000;
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		Double agressiveness = ((tweets_count + friends_count) / diff) / 350;
+		new_user.setAgressiveness(agressiveness);
+		return new_user;
+	}
+
+	public static String[] increaseArray(String[] theArray, String string) {
+		int i = theArray.length;
+		int n = ++i;
+		String[] newArray = new String[n];
+		for (int cnt = 0; cnt < theArray.length; cnt++) {
+			newArray[cnt] = theArray[cnt];
+		}
+		return newArray;
+	}
+
+	public static boolean check_link(String link) throws IOException {
+		
+		String baseURL="https://sb-ssl.google.com/safebrowsing/api/lookup";
+
+		String arguments = "";
+		arguments+=URLEncoder.encode("client", "UTF-8") + "=" + URLEncoder.encode("demo-app", "UTF-8") + "&";
+		arguments+=URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("AIzaSyADggeR7F772IOfbo06hHce3Z8ZqiX4bZY", "UTF-8") + "&";
+		arguments+=URLEncoder.encode("appver", "UTF-8") + "=" + URLEncoder.encode("1.5.2", "UTF-8") + "&";
+		arguments+=URLEncoder.encode("pver", "UTF-8") + "=" + URLEncoder.encode("3.1", "UTF-8")+ "&";
+		arguments+=URLEncoder.encode("url", "UTF-8")+"="+URLEncoder.encode("http://wakux2.com/cs2r/394/", "UTF-8");
+
+		// Construct the url object representing cgi script
+		URL url = new URL(baseURL + "?" + arguments);
+
+		// Get a URLConnection object, to write to POST method
+		URLConnection connect = url.openConnection();
+		//String code = (String) connect.getContentEncoding();
+		InputStreamReader in = new InputStreamReader((InputStream) connect.getContent());
+	    BufferedReader buff = new BufferedReader(in);
+	    String line;
+	    StringBuffer text = new StringBuffer();
+	    boolean is_malware=false;
+	    do {
+	      line = buff.readLine();
+	      text.append(line + "\n");
+	      if (line=="malware"){
+	    	  is_malware=true;
+	      }
+	    	  
+	    } while (line != null);
+	    if (is_malware){
+	    	System.out.println("===========================MALWARE==============================");
+	    }
+	    return is_malware;
+	}
+
+	
+	public static void main(String[] args) throws IOException {
+		AnalyseData analyse = new AnalyseData();
+		analyse.run();
 	}
 }
--- src/twitter_IF25/SearchTweets.java
+++ src/twitter_IF25/SearchTweets.java
@@ -1,102 +1,122 @@
 package twitter_IF25;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+import com.mongodb.util.JSON;
+
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.json.DataObjectFactory;
 
 import java.util.List;
 import java.util.Map;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.Observable;
 
-public class SearchTweets {
-	BufferedWriter OutFileWriter;
+public class SearchTweets extends Observable implements Runnable {
 
-	static final String TERM = "free";
-
-	public static void write(String name, String text) throws IOException {
-		BufferedWriter out = null;
-
-		FileWriter fstream = new FileWriter(name, true); // true tells
-															// to append
-															// data.
-		out = new BufferedWriter(fstream);
-		try {
-			out.write(text + "\n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		out.close();
-	}
+	public String search_key;
 
 	public static void main(String[] args) {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setJSONStoreEnabled(true);
+		SearchTweets search_tweet = new SearchTweets(args[0]);
+		search_tweet.run();
+	}
 
-		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+	public SearchTweets(String search_key) {
+		this.search_key = search_key;
+	}
 
-		try {
-			Query query = new Query(TERM);
-			QueryResult result;
-			do {
-				/*
-				 * if (twitter.getRateLimitStatus() != null) { try {
-				 * Thread.sleep(twitter.getRateLimitStatus()
-				 * .getSecondsUntilReset()); } catch (InterruptedException e) {
-				 * // TODO Auto-generated catch block e.printStackTrace(); }
-				 * 
-				 * }
-				 */
-				Map<String, RateLimitStatus> rateLimitStatus = twitter
-						.getRateLimitStatus();
-				RateLimitStatus status = rateLimitStatus.get("/search/tweets");
-				System.out.println(" Limit: " + status.getLimit());
-				System.out.println(" Remaining: " + status.getRemaining());
-				System.out.println(" ResetTimeInSeconds: "
-						+ status.getResetTimeInSeconds());
-				System.out.println(" SecondsUntilReset: "
-						+ status.getSecondsUntilReset());
-				if (status.getRemaining() < 10) {
-					try {
-						Thread.sleep(status.getSecondsUntilReset() * 1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-
-				result = twitter.search(query);
-				List<Status> tweets = result.getTweets();
-				for (Status tweet : tweets) {
-					// System.out.println("@" + tweet.getUser().getScreenName()
-					// + " - " + tweet.getText());
-
-					System.out.println(TwitterObjectFactory.getRawJSON(tweet));
-					try {
-						SearchTweets.write(TERM+".json",
-								TwitterObjectFactory.getRawJSON(tweet));
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			} while ((query = result.nextQuery()) != null);
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		String search_key = this.search_key;
+		if (search_key == null || search_key == "") {
 			System.exit(0);
-		} catch (TwitterException te) {
-			try {
-				Thread.sleep(1000000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			//te.printStackTrace();
-			//System.out.println("Failed to search tweets: " + te.getMessage());
+		} else {
 
+			MongoClient mongoClient = new MongoClient("localhost");
+
+			DB db = mongoClient.getDB("tweetdata");
+
+			final DBCollection coll_tweets = db.getCollection("tweets");
+
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setJSONStoreEnabled(true);
+
+			Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+
+			try {
+				Query query = new Query(search_key);
+				QueryResult result;
+				do {
+					/*
+					 * if (twitter.getRateLimitStatus() != null) { try {
+					 * Thread.sleep(twitter.getRateLimitStatus()
+					 * .getSecondsUntilReset()); } catch (InterruptedException
+					 * e) { // TODO Auto-generated catch block
+					 * e.printStackTrace(); }
+					 * 
+					 * }
+					 */
+					Map<String, RateLimitStatus> rateLimitStatus = twitter
+							.getRateLimitStatus();
+					RateLimitStatus status = rateLimitStatus
+							.get("/search/tweets");
+					System.out.println(" Limit: " + status.getLimit());
+					System.out.println(" Remaining: " + status.getRemaining());
+					System.out.println(" ResetTimeInSeconds: "
+							+ status.getResetTimeInSeconds());
+					System.out.println(" SecondsUntilReset: "
+							+ status.getSecondsUntilReset());
+					if (status.getRemaining() < 10) {
+						try {
+							Thread.sleep(status.getSecondsUntilReset() * 1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+					result = twitter.search(query);
+					List<Status> tweets = result.getTweets();
+					for (Status tweet : tweets) {
+						// System.out.println("@" +
+						// tweet.getUser().getScreenName()
+						// + " - " + tweet.getText());
+
+						System.out.println(TwitterObjectFactory
+								.getRawJSON(tweet));
+						// try {
+						// SearchTweets.write(TERM+".json",
+						// TwitterObjectFactory.getRawJSON(tweet));
+						// } catch (IOException e) {
+						// // TODO Auto-generated catch block
+						// e.printStackTrace();
+						// }
+						DBObject dbObject = (DBObject) JSON
+								.parse(TwitterObjectFactory.getRawJSON(tweet));
+
+						coll_tweets.insert(dbObject);
+
+					}
+				} while ((query = result.nextQuery()) != null);
+				//System.exit(0);
+			} catch (TwitterException te) {
+				try {
+					Thread.sleep(1000000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
 		}
 	}
 }
